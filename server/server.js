6ca78e25a9c1597b79e67b44bdcfdcdbdcab577f
adityapanwar027 <adityapanwar027@gmail.com>
2026-06-28 10:34:52 +0530

    Implement JWT authentication system

--- server/server.js
+++ server/server.js
@@ -2,45 +2,62 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const helmet = require("helmet");
-const morgon = require("morgan");
-const connectDB = require("./config/db")
+const morgan = require("morgan");
 
-// Load environment variables
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const authMiddleware = require("./middleware/authMiddleware");
+
+// Load Environment Variables
 dotenv.config();
+
+// Connect Database
 connectDB();
 
-// Create express app
+// Initialize Express App
 const app = express();
 
-//Security middleware
+// Security Middleware
 app.use(helmet());
 
-// Enable cors
+// CORS Middleware
 app.use(
-    cors({
-        origin: "http://localhost:5173",
-        credentials: "true",
-    })
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
 );
 
 // Body Parser Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // Logger Middleware
-app.use(morgon("dev"));
+app.use(morgan("dev"));
 
-// Test Route
+// Health Check Route
 app.get("/", (req, res) => {
-    res.status(200).json({
-        success: true,
-        message : "ProjectPilot AI API is running"
-    });
+  res.status(200).json({
+    success: true,
+    message: "ProjectPilot AI API is Running 🚀",
+  });
+});
+
+// Authentication Routes
+app.use("/api/auth", authRoutes);
+
+// Protected Route
+app.get("/api/profile", authMiddleware, (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Protected Route Accessed Successfully",
+    user: req.user,
+  });
 });
 
 // Start Server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server is rinnung on http://localhost:${PORT}`);
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
